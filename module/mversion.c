e296d3a55dc9b9b95e1eb5d99ad3bcaccde72ff2
mo <jgb40@qq.com>
2017-05-03 09:41:14 +0800

    修改mversion变量命名

--- module/mversion.c
+++ module/mversion.c
@@ -2,39 +2,41 @@
 
 #include "mversion.h"
 
+
+const char *months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+
+
 /*
 * __DATE__ : May 12 2017  月日年
 * __TIME__ : 15:26:26     时分秒
 *
 */
-const char *months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-
 uint32_t mver_getver(void)
 {
     uint8_t i,lengh;
 		uint32_t ver;
-    char *pdt = __DATE__;
+    char *pstr = __DATE__;
 
-    lengh = strlen(pdt);
-    ver = pdt[lengh - 4] - 0x30;
-    ver = ver * 10 + pdt[lengh - 3] - 0x30;
-    ver = ver * 10 + pdt[lengh - 2] - 0x30;
-    ver = ver * 10 + pdt[lengh - 1] - 0x30;
+    lengh = strlen(pstr);
+    ver = pstr[lengh - 4] - 0x30;
+    ver = ver * 10 + pstr[lengh - 3] - 0x30;
+    ver = ver * 10 + pstr[lengh - 2] - 0x30;
+    ver = ver * 10 + pstr[lengh - 1] - 0x30;
     	
     for(i = 0;i < sizeof(months)/sizeof(months[0]); i++){
-        if(memcmp(months[i],pdt,3) == 0){
+        if(memcmp(months[i],pstr,3) == 0){
             ver = ver * 100 + i + 1;
             break;
         }
     }
 
     lengh = 4;
-    ver = ver * 10 + (pdt[lengh] == ' ' ? 0 : (pdt[lengh] - 0x30));
-    ver = ver * 10 + pdt[lengh + 1] - 0x30;
+    ver = ver * 10 + (pstr[lengh] == ' ' ? 0 : (pstr[lengh] - 0x30));
+    ver = ver * 10 + pstr[lengh + 1] - 0x30;
 
-    pdt = __TIME__;
-    ver = ver * 10 + (pdt[0] == ' ' ? 0 : (pdt[0] - 0x30));
-    ver = ver * 10 + pdt[1] - 0x30;
+    pstr = __TIME__;
+    ver = ver * 10 + (pstr[0] == ' ' ? 0 : (pstr[0] - 0x30));
+    ver = ver * 10 + pstr[1] - 0x30;
     
     return ver;
 }
